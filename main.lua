3d1ea02f39416cf7753754d164c407b123d8931e
squale <squalinho@gmail.com>
2022-08-14 15:53:46 +0200

    ajout code de base

---
+++ main.lua
@@ -0,0 +1,15 @@
+io.stdout:setvbuf("no")
+
+love.graphics.setDefaultFilter("nearest")
+
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
+
+function love.keypressed(key)
+end
